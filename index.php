ae496574a6627581c9aac331fa57cb5cec0f9ff2
wandersonreiss <wreletronicawr@gmail.com>
2023-02-08 16:51:22 -0300

    calculadora

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>calculadora</title>
+    </head>
+    <body>
+        <div class="fundo">
+            <h1>deson</h1>
+            <div class="calculadora">
+                <h1>calculadora</h1>
+                
+            </div>
+            
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
