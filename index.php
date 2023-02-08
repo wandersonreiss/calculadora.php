df542c111eae1f62c75398351f06e2e1c8670104
wandersonreiss <wreletronicawr@gmail.com>
2023-02-08 17:08:18 -0300

    atualizaçao

--- index.php
+++ index.php
@@ -14,7 +14,22 @@ and open the template in the editor.
             <h1>deson</h1>
             <div class="calculadora">
                 <h1>calculadora</h1>
-                
+                <table>
+                    <tr>
+                        <td><button>c</button></td>
+                         <td><button><</button></td>
+                          <td><button>\</button></td>
+                           <td><button>x</button></td>
+                    </tr>
+                </table>
+                 <table>
+                    <tr>
+                        <td><button>7</button></td>
+                         <td><button>8</button></td>
+                          <td><button>9</button></td>
+                           <td><button>-</button></td>
+                    </tr>
+                </table>
             </div>
             
         </div>
