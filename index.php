0828eab7c0b5048fe1afe45fa149757140a2e424
wandersonreiss <wreletronicawr@gmail.com>
2023-02-09 09:04:38 -0300

    atualizaçao

--- index.php
+++ index.php
@@ -30,6 +30,28 @@ and open the template in the editor.
                            <td><button>-</button></td>
                     </tr>
                 </table>
+                 <table>
+                    <tr>
+                        <td><button>4</button></td>
+                         <td><button>5</button></td>
+                          <td><button>6</button></td>
+                           <td><button>+</button></td>
+                    </tr>
+                </table>
+                <table>
+                    <tr>
+                        <td><button>1</button></td>
+                         <td><button>2</button></td>
+                          <td><button>3</button></td>
+                          <td rowspan="2"><button>=</button></td>
+                    </tr>
+                </table>
+                 <table>
+                    <tr>
+                        <td colspan="2"><button>0</button></td>
+                         <td><button>.</button></td>
+                    </tr>
+                </table>
             </div>
             
         </div>
